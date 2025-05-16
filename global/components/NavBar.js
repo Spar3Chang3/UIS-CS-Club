d7fef29e7dddc650607af9535cfeac3353376925
spar3chang3 <darykdbaker@gmail.com>
2025-05-16 15:57:40 -0500

    Add pictures, update home page, change navbar slightly

--- global/components/NavBar.js
+++ global/components/NavBar.js
@@ -40,6 +40,37 @@ function initNavbar(links, logoSrc, styleSrc) {
     toggle.addEventListener("click", () => {
         linksContainer.classList.toggle("open");
     });
+
+    const topBtn = document.createElement("button");
+    topBtn.className = "top-btn";
+    topBtn.innerHTML = `
+        <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+          <line x1="24" y1="20" x2="24" y2="44" stroke="white" stroke-width="2" />
+          <polyline points="18,20 24,8 30,20" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    `
+
+    topBtn.style.opacity = "0";
+    topBtn.style.visibility = "hidden";
+
+    topBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // optional — makes the scroll animated
+        });
+    });
+
+    document.body.appendChild(topBtn);
+
+    document.addEventListener("scroll", () => {
+        if (window.scrollY > 160) {
+            topBtn.style.opacity = "1";
+            topBtn.style.visibility = "visible";
+        } else {
+            topBtn.style.opacity = "0";
+            topBtn.style.visibility = "hidden";
+        }
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => initNavbar(NavLinks, CheckColorScheme(Logos.club), "/global/components/NavBar.css"));
---
+++ home/home.js
@@ -0,0 +1,32 @@
+function runIntroObserver(observerWrapper) {
+    const children = observerWrapper.querySelectorAll(".intro-text, .events");
+    for (const child of children) {
+        console.log(child);
+        child.style = "transform: translateY(0); opacity: 1; visibility: visible;";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+   const targets = new Map([
+       ["observer-wrapper", false]
+   ]);
+
+   const observer = new IntersectionObserver((entries) => {
+       for (const entry of entries) {
+           const id = entry.target.id;
+           console.log(id);
+           if (!entry.isIntersecting || targets.get(id)) return;
+
+           switch (id) {
+               case "observer-wrapper":
+                   runIntroObserver(entry.target);
+                   targets.set(id, true);
+           }
+       }
+   });
+
+    targets.forEach((_, id) => {
+        const el = document.getElementById(id);
+        if (el) observer.observe(el);
+    });
+});
